005776d542c28e89bc4459bd7207694f967118a9
Lucía <luciaa.martinezrivas@gmail.com>
2022-07-16 12:52:34 +0200

    añadir nuevo archivo borderEffects lucia

--- Proyecto CSS/Javascript/index.js
+++ Proyecto CSS/Javascript/index.js
@@ -3,27 +3,9 @@ import { showTime } from "./showTime.js"
 
 document.addEventListener('DOMContentLoaded', showTime)
 
-function borderEffects() {
+import { borderEffects } from "./borderEffects.js"
 
-    const divsWeb = document.querySelectorAll('div, section')
-    console.log(divsWeb)
-
-    for (const div of divsWeb) {
-
-        div.addEventListener('mouseover', (event) => {
-            if (event.altKey) {
-                div.style.border = '1px solid red';
-            }
-        })
-
-        div.addEventListener('mouseout', (event) => {
-            if (event.altKey) {
-                div.style.border = null;
-            }
-        })
-
-    }
-}
+document.addEventListener('load', borderEffect)
 
 function EvaluarCaracter(evObject) {
     let Caracter = String.fromCharCode(evObject.which);
